dababdc5b8355ee3b5f3f00798325a7ba691af5f
Kai <b10223052@yuntech.org.tw>
2018-06-29 13:38:17 +0800

    compass in file

--- Kai/config.rb
+++ Kai/config.rb
@@ -1,3 +1,4 @@
+require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -11,7 +12,7 @@ javascripts_dir = "javascripts"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
