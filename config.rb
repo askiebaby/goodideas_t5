f1cc2283c859ee2e0277f78b9bb3b9799b36eeeb
Askie Lin <isme408@gmail.com>
2019-01-07 23:34:00 +0800

    Removed other folders

--- config.rb
+++ config.rb
@@ -2,20 +2,21 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "./"
-css_dir = "Chris/stylesheets"
-sass_dir = "Chris/sass"
-images_dir = "Chris/images"
-javascripts_dir = "Chris/javascripts"
+http_path = "/"           # 你的根目錄位置
+css_dir = "css"           # 你的CSS資料夾路徑
+sass_dir = "sass"         # 你的Sass資料夾路徑
+images_dir = "images"     # 你的圖片資料夾路徑
+javascripts_dir = "js"    # 你的js資料夾路徑
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
